2fadc3b76f2c53de023fa5de0d984597b1e148e2
joohyungjin <hjjooace24@gmail.com>
2019-09-25 16:59:46 +0900

    네이버

---
+++ C++/취업/네이버/Naver3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string.h>
+#include <string>
+
+using namespace std;
+
+vector <string>solution(vector<string>);
+int main(void) {
+	vector<int> input;
+	int sum = 0;
+
+	return 0;
+}
+vector < string> solution(vector<string> record) {
+	vector<string>answer;
+	return answer;
+}
