d893ba9df012f09fc929db4b32605dd38e2b6fdf
Hyungjin Joo <hjjooace@naver.com>
2019-10-30 20:03:27 +0900

    JAVA 첫 커밋

---
+++ JAVA/SW Expert/두문자어_8741.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class 두문자어_8741 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String message;
+		Scanner scan = new Scanner(System.in);
+
+		int T = scan.nextInt();
+		scan.nextLine();
+		for (int i = 0; i < T; i++) {
+			message = scan.nextLine();
+			String result = Name(message);
+			System.out.println("#" + (i + 1) + " " + result);
+		}
+	}
+
+	public static String Name(String message) {
+		String data[] = message.split(" ");
+		String result = "";
+		for (int i = 0; i < 3; i++) {
+			result += data[i].substring(0, 1).toUpperCase();
+		}
+		return result;
+	}
+}
